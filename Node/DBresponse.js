db548fd7b15dcecbf3b1880aaf4d7a6e48a982da
rumendo <rumen_do@abv.bg>
2018-07-02 13:45:32 +0300

    Improved ajax performance. Sorted countries and cities.

--- Node/DBresponse.js
+++ Node/DBresponse.js
@@ -13,10 +13,50 @@ con.connect(function(err) {
   if (err) throw err;
   // Select all venues and return the result to be sent.
     app.get('/', function(req, res) {
-      con.query('SELECT * FROM markers WHERE country = ?', [req.param('country')], function (err, result, fields) {
-        if (err) throw err;
-      res.send(result);
-      });
+      if(req.param('load')){
+        con.query('SELECT DISTINCT country FROM markers', function (err, result, fields) {
+          if (err) throw err;
+        countries = [];
+        i=0;
+        Array.prototype.forEach.call(result, function(element){
+          countries[i] = element.country;
+          i++;
+        });
+        countries.sort();
+        res.send(countries);
+        });
+      }
+
+      if(req.param('country')){
+        con.query('SELECT DISTINCT city FROM markers WHERE country = ?', [req.param('country')], function (err, result, fields) {
+          if (err) throw err;
+        cities = [];
+        i=0;
+        Array.prototype.forEach.call(result, function(element){
+          cities[i] = element.city;
+          i++;
+        });
+        cities.sort();
+        res.send(cities);
+        });
+      }
+      
+      if(req.param('selCountry')){
+        if(req.param('selCity')){
+          var selCountry = req.param('selCountry');
+          var selCity = req.param('selCity');
+          console.log(selCity);
+          con.query('SELECT * FROM markers WHERE city = ?', [selCity], function (err, result, fields) {
+          if (err) throw err;
+          res.send(result);
+          });
+        }else{
+          con.query('SELECT * FROM markers WHERE country = ?', [req.param('selCountry')], function (err, result, fields) {
+          if (err) throw err;
+          res.send(result);
+          });
+        }
+      }
     });
   });
 
--- wordpress/map.php
+++ wordpress/map.php
@@ -13,24 +13,88 @@
         padding: 0;
       }
     </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   </head>
 
-  <body>
+  <body onload="readCC();">
     <form>
-      <input type="radio" name="gender" value="BG" checked onClick="initMap('BG');"> Bulgaria<br>
-      <input type="radio" name="gender" value="GB" onClick="initMap('GB');"> Great Britan<br>
-      <input type="radio" name="gender" value="US" onClick="initMap('US');"> USA<br>
+      <input name="country" list="country" id="CC">
+      <datalist id="country"></datalist>
+      <input name="city" list="city" id="cities">
+      <datalist id="city"></datalist>
     </form>
+    <div id="countryList"></div>
     <div id="map"></div>
     <script>
-        function initMap(country) {
+      var selCountry = 0;
+      var selCity = 0;
+
+      function readCC(){
+        var url = 'http://localhost:3000/?load=1';
+          downloadUrl(url, function(result) {
+            displayCC(result);
+        });
+      }
+
+      function readCities(country){
+        var url = 'http://localhost:3000/?country=' + country;
+        downloadUrl(url, function(result) {
+          displayCities(result);
+        });
+      }
+
+
+      timer2 = 0;
+      function selectCountry (){
+        readCities(selCountry);
+      }
+      $("#CC").on('input', function(e){
+        selCountry = this.value;
+        console.log(selCity);
+          if (timer2) {
+              clearTimeout(timer2);
+          }
+          timer2 = setTimeout(selectCountry, 500);
+      });
+
+      timer = 0;
+      function selectCity (){
+          initMap();
+      }
+      $("#cities").on('input', function(e){
+        selCity = this.value;
+        console.log(selCity);
+          if (timer) {
+              clearTimeout(timer);
+          }
+          timer = setTimeout(selectCity, 500);
+      });
+
+      function displayCC(result){
+        var options = '';
+        Array.prototype.forEach.call(result, function(element){
+          options += '<option value="'+element+'"/>';
+        });
+        document.getElementById('country').innerHTML = options;
+      }
+
+      function displayCities(result){
+        var options = '';
+        Array.prototype.forEach.call(result, function(element){
+          options += '<option value="'+element+'"/>';
+        });
+        document.getElementById('city').innerHTML = options;
+      }
+
+
+      function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           center: new google.maps.LatLng(42.650692, 23.342963),
           zoom: 2
         });
         var infoWindow = new google.maps.InfoWindow;
         var url = 'http://localhost:3000';
-        var data = { 'country': country };
+        var data = { 'selCountry': selCountry, 'selCity': selCity};
         var querystring = encodeQueryData(data);
         url = url.concat("/?" + querystring);
         console.log(url);
